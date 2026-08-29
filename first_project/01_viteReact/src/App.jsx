8431e25e6dbe110677d1315f7823758cb84a3d48
SoumyaVartak <saumyavartak4@gmail.com>
2026-08-29 17:09:49 +0530

    added

---
+++ first_project/01_viteReact/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Chai from "./Chai"
+function App() {
+
+
+  return (
+    // THIS<></> IS CALLED AS FRAGMENTS AND IT ALSO CALLED AS DIV
+     <>
+  <Chai/>
+  <h1>chai aur code</h1>
+  <p>paragh </p>
+  </>
+  )
+}
+
+export default App
